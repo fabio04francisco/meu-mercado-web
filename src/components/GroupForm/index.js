03ba0ee4f8203e865a2af3b2af16222237dcf64f
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-02-21 22:26:22 -0300

    feat: criado componente GroupForm

---
+++ src/components/GroupForm/index.js
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+import { Container } from './styles';
+
+export default function GroupForm({ children }) {
+  return (
+    <Container>
+      {children}
+    </Container>
+  );
+}
