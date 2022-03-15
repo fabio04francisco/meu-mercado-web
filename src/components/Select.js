5c1cb7d92bb6caa2a9debdf3019988bc3b1693f6
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-03-14 23:37:34 -0300

    feat: criado componente Select

---
+++ src/components/Select.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+export default styled.select`
+  width: 100%;
+  border: none;
+  background: ${({ theme }) => theme.colors.background};
+  border: 1px solid ${({ theme }) => theme.colors.gray[200]};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  border-radius: 7px;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+  transition: border-color 0.2s ease-in;
+  appearance: none;
+
+  &:focus{
+    border-color:${({ theme }) => theme.colors.primary.main};
+  }
+`;
