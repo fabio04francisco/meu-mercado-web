4b57b590b99f1c836cefb15d827cd1ab54e1bbfe
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-03-24 23:00:12 -0300

    feat: vinculado inputs ao state de controle das informações

--- src/pages/SignIn/StepName/index.js
+++ src/pages/SignIn/StepName/index.js
@@ -5,50 +5,61 @@ import Button from '../../../components/Button';
 
 import { Container } from './styles';
 
-export default function StepName({ fields, handleFieldChange, handleClickStep }) {
+export default function StepName({
+  fields, handleFieldChange, handleClickStep, errors, getErrorMessageByFieldName,
+}) {
   const {
     name, email, phone, password,
   } = fields;
+
+  const isStepNameValid = (Boolean(name && email && phone && password) && errors.length === 0);
+
   return (
     <Container className="step">
-      <GroupForm>
+      <GroupForm error={getErrorMessageByFieldName('name')}>
         <Input
           name="name"
           value={name}
-          placeholder="Nome"
+          placeholder="Nome *"
           onChange={handleFieldChange}
+          error={getErrorMessageByFieldName('name')}
         />
       </GroupForm>
-      <GroupForm>
+      <GroupForm error={getErrorMessageByFieldName('email')}>
         <Input
           name="email"
           value={email}
-          placeholder="E-mail"
+          placeholder="E-mail *"
           onChange={handleFieldChange}
+          error={getErrorMessageByFieldName('email')}
         />
       </GroupForm>
 
-      <GroupForm>
+      <GroupForm error={getErrorMessageByFieldName('phone')}>
         <Input
           name="phone"
+          maxLength="15"
           value={phone}
-          placeholder="Telefone"
+          placeholder="Telefone *"
           onChange={handleFieldChange}
+          error={getErrorMessageByFieldName('phone')}
         />
       </GroupForm>
 
-      <GroupForm>
+      <GroupForm error={getErrorMessageByFieldName('password')}>
         <Input
           type="password"
           name="password"
           value={password}
-          placeholder="Senha"
+          placeholder="Senha *"
+          error={getErrorMessageByFieldName('password')}
           onChange={handleFieldChange}
         />
       </GroupForm>
 
       <Button
         onClick={() => handleClickStep(1)}
+        disabled={!isStepNameValid}
       >
         Próximo
       </Button>
