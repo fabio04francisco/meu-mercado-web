13efa6337a1c4c4fb8f966be994e7f0eefa1a2e2
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-03-24 23:03:15 -0300

    feat: add regras de validação das entradas.

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -8,8 +8,13 @@ import logo from '../../assets/images/logo.svg';
 import StepName from './StepName';
 import StepAdress from './StepAdress';
 
+import isEmailValid from '../../utils/isEmailValid';
+import formatPhone from '../../utils/formatPhone';
+import formatCEP from '../../utils/formatCEP';
+
 import { useFormFields } from '../../hooks/useFormFields';
-// import useErrors from '../../hooks/useErrors';
+import useErrors from '../../hooks/useErrors';
+import isCEPValid from '../../utils/isCEPValid';
 
 const initialFormFields = {
   name: '',
@@ -19,19 +24,19 @@ const initialFormFields = {
   adress: '',
   district: '',
   city: '',
-  state: 'SC',
+  state: '',
   cep: '',
 };
 export default function Login() {
-  const [fields, handleFieldChange] = useFormFields(initialFormFields);
+  const [fields, setFields] = useFormFields(initialFormFields);
   const [step, setStep] = useState(1);
 
-  // const {
-  //   errors,
-  //   setError,
-  //   removeError,
-  //   getErrorMessageByFieldName,
-  // } = useErrors();
+  const {
+    errors,
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+  } = useErrors();
 
   const handleNextStep = useCallback(() => {
     setStep((prevState) => prevState + 1);
@@ -40,6 +45,66 @@ export default function Login() {
   const handleBackStep = useCallback(() => {
     setStep((prevState) => prevState - 1);
   }, []);
+
+  function handleFieldChange(event) {
+    const inputName = event.target.name;
+    let inputValue = event.target.value;
+
+    const inputNameDescription = {
+      name: 'Nome',
+      adress: 'Endereço',
+      district: 'Bairro',
+      city: 'Cidade',
+      state: 'Estado',
+    };
+
+    switch (inputName) {
+      case 'name':
+      case 'adress':
+      case 'district':
+      case 'city':
+      case 'state':
+        if (!inputValue) {
+          setError({ field: inputName, message: `${inputNameDescription[inputName]} é obrigatório.` });
+        } else {
+          removeError(inputName);
+        }
+        break;
+
+      case 'email':
+        if (inputValue && !isEmailValid(inputValue)) {
+          setError({ field: 'email', message: 'Email inválido.' });
+        } else {
+          removeError('email');
+        }
+        break;
+
+      case 'phone':
+        inputValue = formatPhone(inputValue);
+        break;
+
+      case 'password':
+        if (inputValue.length < 6) {
+          setError({ field: 'password', message: 'Senha minimo 6 caracteres' });
+        } else {
+          removeError('password');
+        }
+        break;
+
+      case 'cep':
+        inputValue = formatCEP(inputValue);
+
+        if (inputValue && !isCEPValid(inputValue)) {
+          setError({ field: 'cep', message: 'CEP inválido.' });
+        } else {
+          removeError('cep');
+        }
+        break;
+      default: return;
+    }
+
+    setFields({ name: inputName, value: inputValue });
+  }
 
   function handleFormSubmit(event) {
     event.preventDefault();
@@ -66,6 +131,8 @@ export default function Login() {
           ? (
             <StepName
               fields={fields}
+              errors={errors}
+              getErrorMessageByFieldName={getErrorMessageByFieldName}
               handleFieldChange={handleFieldChange}
               handleClickStep={handleNextStep}
             />
@@ -73,6 +140,8 @@ export default function Login() {
           : (
             <StepAdress
               fields={fields}
+              errors={errors}
+              getErrorMessageByFieldName={getErrorMessageByFieldName}
               handleFieldChange={handleFieldChange}
               handleClickStep={handleBackStep}
               handleFormSubmit={handleFormSubmit}
