efd20fbffb9fdef8f9b329e7b6eef3a13cb812fb
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-04-01 00:05:48 -0300

    feat: criado rota para pagina home

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import Login from './pages/Login';
 import SignIn from './pages/SignIn';
+import Home from './pages/Home';
 
 export default function MainRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/signin" element={<SignIn />} />
+      <Route path="/home" element={<Home />} />
     </Routes>
   );
 }
