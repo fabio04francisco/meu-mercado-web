592648503856b657bbf36497ff94c2fdf7402c14
Fabio Francisco <fabinhofrancisco@gmail.com>
2022-04-25 23:51:26 -0300

    feat: add rota catalado de mercados

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Login from './pages/Login';
 import SignIn from './pages/SignIn';
 import Home from './pages/Home';
+import Catalog from './pages/Catalog';
 
 export default function MainRoutes() {
   return (
@@ -10,6 +11,7 @@ export default function MainRoutes() {
       <Route path="/" element={<Login />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/catalog" element={<Catalog />} />
     </Routes>
   );
 }
